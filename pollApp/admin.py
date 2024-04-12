a23f10b8eeb201c32e98f8278d7e1a33662957d3
Riyan Jamil <riyanjamil220@gmail.com>
2024-04-12 08:51:08 +0500

    Created question and choice model and customized and registered models in admin

--- pollApp/admin.py
+++ pollApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.site_header = 'The Poll Mall'
+admin.site.site_title = 'Voting admin area'
+admin.site.index_title = 'Welcome to our voting admin area'
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice 
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields':['question_text']}),
+                 ('Date Information', {'fields':['publishing_date'] ,'classes':['collapse']}),
+                 ]
+    
+    inlines = [ChoiceInLine]
+
+admin.site.register(Question, QuestionAdmin)
