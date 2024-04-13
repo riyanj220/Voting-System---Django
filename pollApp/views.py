37fff73780dbe93b0d5e21458711e8bc3a3583ec
Riyan Jamil <riyanjamil220@gmail.com>
2024-04-13 08:05:53 +0500

    Cleaned up the project little bit

--- pollApp/views.py
+++ pollApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render , get_object_or_404
-from django.http import HttpResponse , HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import Http404
 from .models import *
